67a8dfd165767bf2e120b07f679ded8ba034a88e
Christian Ngabi <krysngabi@gmail.com>
2026-09-07 19:50:48 -0500

    Add more fields to EmergencyCallStatusEvent

--- notifications/src/main/java/com/abovebytes/notifications/models/websocket/emergency/EmergencyCallStatusEvent.java
+++ notifications/src/main/java/com/abovebytes/notifications/models/websocket/emergency/EmergencyCallStatusEvent.java
@@ -1,9 +1,11 @@
 package com.abovebytes.notifications.models.websocket.emergency;
 
+import com.abovebytes.models.emergency.CallStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 /**
  * Envelope published onto {@link com.abovebytes.notifications.constants.RabbitConstants#EMERGENCY_EXCHANGE}
@@ -17,14 +19,36 @@ import java.io.Serializable;
 public class EmergencyCallStatusEvent implements Serializable {
 
     private Long userId;
+    private UUID emergencyCallId;
+
+    private Long dispatcherId;
+    private String dispatcherName;
+
+    private CallStatus status;
     private Object payload;
 
     public EmergencyCallStatusEvent() {
         // required for Jackson deserialization on the consumer side
     }
 
-    public EmergencyCallStatusEvent(Long userId, Object payload) {
+    public EmergencyCallStatusEvent(Long userId, UUID emergencyCallId, Long dispatcherId, String dispatcherName, CallStatus status, Object payload) {
         this.userId = userId;
+        this.emergencyCallId = emergencyCallId;
+        this.dispatcherId = dispatcherId;
+        this.dispatcherName = dispatcherName;
+        this.status = status;
         this.payload = payload;
+    }
+
+    @Override
+    public String toString() {
+        return "EmergencyCallStatusEvent{" +
+                "userId=" + userId +
+                ", emergencyCallId=" + emergencyCallId +
+                ", dispatcherId=" + dispatcherId +
+                ", dispatcherName='" + dispatcherName + '\'' +
+                ", status=" + status +
+                ", payload=" + payload +
+                '}';
     }
 }
